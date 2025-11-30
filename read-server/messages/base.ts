524fcb1822b9d4964c9166c596021ee169194e54
Mikihiro SUDA <mikihiro.suda@kubell.com>
2025-11-30 17:49:35 +0900

    reset

--- read-server/messages/base.ts
+++
@@ -1,6 +0,0 @@
-export interface BaseMessage {
-  version: number;
-  type: string;
-  commitTs: number;
-  buildTs: number;
-}
--- read-server/messages/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ChatRoomCreatedMessage } from "./chatRoom.ts";
-import type { MemberCreatedMessage } from "./member.ts";
-import type { TiCDCSystemMessage } from "./ticdcSystem.ts";
-
-export type Message =
-  | TiCDCSystemMessage
-  | ChatRoomCreatedMessage
-  | MemberCreatedMessage;
