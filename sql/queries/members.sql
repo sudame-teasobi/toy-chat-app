b653d3e599f6d5c28eab3a6be87b3f73d4b83109
Mikihiro SUDA <mikihiro.suda@kubell.com>
2026-01-21 17:30:20 +0900

    add docs

--- sql/queries/members.sql
+++ sql/queries/members.sql
@@ -8,3 +8,5 @@ SELECT * FROM chat_room_members WHERE chat_room_id = ?;
 -- name: CreateMember :execresult
 INSERT INTO chat_room_members (id, user_id, chat_room_id) VALUES (?, ?, ?);
 
+-- name: GetMemberByChatRoomAndUser :one
+SELECT * FROM chat_room_members WHERE chat_room_id = ? AND user_id = ? LIMIT 1;
