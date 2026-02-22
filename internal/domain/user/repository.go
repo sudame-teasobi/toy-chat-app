7bdf153e32684b507464e847d35f57df1741e634
Mikihiro SUDA <mikihiro.suda@kubell.com>
2026-02-22 16:00:21 +0900

    fix: #57

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -4,6 +4,6 @@ import "context"
 
 // Repository defines the interface for User persistence.
 type Repository interface {
-	Save(ctx context.Context, chatRoom *User) error
+	Save(ctx context.Context, user *User) error
 	FindByID(ctx context.Context, id string) (*User, error)
 }
